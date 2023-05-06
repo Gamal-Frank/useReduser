c3e4741cc96a1b9885489db1466a3a43a664a209
frank <gamalgamer10@yahoo.com>
2023-05-06 16:15:56 +0300

    added input

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useReducer } from "react";
 
 function App() {
-
-  return (
-    <div></div>
-  )
+  const [state, dispatch] = useReducer(
+    (state,action) => {
+      switch (action.type) {
+        case "setName":
+          return {...state, user: action.payload };
+        case "addName":
+          return { ...state, names: [...state.names, action.paylod] };
+          default: return state
+      }
+    },
+    {
+      names: [],
+     user:"frank",
+    }
+  );
+  return <div>
+    <div>{state.user}</div>
+    <input type="text" onChange={(e)=>dispatch({type:"setName",payload:e.target.value})} />
+  </div>;
 }
 
-export default App
+export default App;
